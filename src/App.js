0d85325c0ebaa12d69e39257d3270063922bce97
JA <jalcornaoz@gmail.com>
2023-02-28 12:55:34 +0100

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,41 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import "./App.css";
 import MostrarMensaje from "./MostrarMensaje.js";
 
+const randomNumber = () => Math.trunc(Math.random() * 20) + 1;
+
 function App() {
-  const randomNumber = () => Math.trunc(Math.random() * 20) + 1;
   const [number, setNumber] = useState("");
   const [score, setScore] = useState(20);
   const [highscore, setHighscore] = useState(0);
   const [secretNumber, setSecretNumber] = useState(randomNumber());
+  const inputRef = useRef(null);
 
   const handleCheck = () => {
-    if (number === secretNumber) {
-      // mensaje: has ganado
-      // mostrar número secreto -> HECHO
-      // si 'score' mayor que 'highscore', actualizar el nombre
-    } else if (number > secretNumber) {
-      // mensaje: el número secreto es menor
-      // disminuir score
-    } else if (number < secretNumber) {
-      // mensaje: el número secreto es mayor
-      // disminuir score
-    }
-    setScore(score - 1);
-    setHighscore(highscore + 1);
+    setNumber(Number(inputRef.current.value));
   };
+
+  useEffect(() => {
+    console.log(`El número secreto es ${secretNumber}`);
+    console.log(`El número introducido es ${number}`);
+    if (number === secretNumber) {
+      // mensaje: has ganado -> HECHO
+      // mostrar número secreto -> HECHO
+      // cambiar el color de fondo (verde)
+      // si 'score' mayor que 'highscore', actualizar el highscore
+    } else if (number === 1) {
+      setScore(score - 1);
+      // mensaje: has perdido -> HECHO
+      // cambiar el color de fondo (rojo)
+    } else {
+      // mensaje: el número secreto es mayor o menor -> HECHO
+      setScore(score - 1);
+    }
+  }, [number]);
 
   return (
     <div>
+      {console.log("Renderizando App")}
       <header>
         <h1>Guess My Number!</h1>
         <p className="between">(Between 1 and 20)</p>
@@ -35,7 +44,7 @@ function App() {
       </header>
       <main>
         <section className="left">
-          <input type="number" className="guess" />
+          <input type="number" className="guess" ref={inputRef} />
           <button className="btn check" onClick={handleCheck}>
             Check!
           </button>
--- src/MostrarMensaje.js
+++ src/MostrarMensaje.js
@@ -1,10 +1,14 @@
+import "./MostrarMensaje.css";
+
 export default function MostrarMensaje({ number, secretNumber }) {
+  console.log(number, typeof number, secretNumber, typeof secretNumber);
   if (number === secretNumber) {
     return <p className="message">¡Has acertado!</p>;
+  } else if (number === "") {
+    return <p className="message">Empezamos a jugar...</p>;
   } else if (number > secretNumber) {
     return <p className="message">Numero secreto menor.</p>;
   } else if (number < secretNumber) {
     return <p className="message">Numero secreto mayor.</p>;
   }
-  return <p className="message">Adivina el número...</p>;
 }
